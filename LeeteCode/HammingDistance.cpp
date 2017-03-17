7814d9b69e6373cf52905820adda40a8f129bf72
DerCP <DerCP@DESKTOP-1BH0TB8>
2017-03-17 23:43:31 +0800

    Daily Exercise

--- LeeteCode/HammingDistance.cpp
+++ LeeteCode/HammingDistance.cpp
@@ -4,6 +4,7 @@
 
 /*
 **
+**	461. Hamming Distance 
 **	https://leetcode.com/problems/hamming-distance/
 **	The Hamming distance between two integers is the number of positions 
 **	at which the corresponding bits are different.
