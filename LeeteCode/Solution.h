8e7eeb417648d48ed6440a30b468572a8f576f6d
DerCP <DerCP@DESKTOP-1BH0TB8>
2017-03-18 16:22:09 +0800

    Daily Exercise 3/18 2017

--- LeeteCode/Solution.h
+++ LeeteCode/Solution.h
@@ -4,7 +4,10 @@ class Solution
 public:
 	Solution();
 	~Solution();
+	//461. Hamming Distance
 	int hammingDistance(int x, int y);
+
+	//412. Fizz Buzz
 	std::vector<std::string> fizzBuzz(int n);
 };
 
